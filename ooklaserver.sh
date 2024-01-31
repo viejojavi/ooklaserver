0c81e3149097b4f1b1bab32fd0000f0b1abd29c3
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-01-30 22:57:46 -0500

    Update ooklaserver.sh

--- ooklaserver.sh
+++ ooklaserver.sh
@@ -1,35 +1,5 @@
 #!/bin/sh
-#Reviso si el directorio existe
-if [ -d "/etc/systemd/system" ] 
-then
-    if [ -f "/etc/systemd/system/OoklaServer.service" ];
-    then
-    	[Unit] 
- 		Description=OoklaServer start  
- 		After=network.target
-  
-	[Service] 
-		User=root 
- 		ExecStart=/home/ticcol/OoklaServer 
-  
- 	[Install] 
- 		WantedBy=multi-user.target
-    	systemctl enable OoklaServer.service
- 	sleep 5
-	systemctl start OoklaServer.service
-    fi
-else
-	[Unit] 
- 		Description=OoklaServer start  
- 		After=network.target
-  
-	[Service] 
-		User=root 
- 		ExecStart=/home/ticcol/OoklaServer 
-  
- 	[Install] 
- 		WantedBy=multi-user.target
-    	systemctl enable OoklaServer.service
- 	sleep 5
-	systemctl start OoklaServer.service
-fi
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/viejojavi/ooklaserver/main/OoklaServer.service
+systemctl enable OoklaServer.service
+systemctl start OoklaServer.service
