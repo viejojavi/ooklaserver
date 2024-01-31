06a6aa927265f7fe0899b16ebf433fad65b88e5c
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-01-30 23:41:57 -0500

    Update ooklaserver.sh

--- ooklaserver.sh
+++ ooklaserver.sh
@@ -16,3 +16,5 @@ cd /etc/systemd/system
 wget https://raw.githubusercontent.com/viejojavi/ooklaserver/main/OoklaServer.service
 systemctl enable OoklaServer.service
 systemctl start OoklaServer.service
+sleep 5
+echo "SERVIDOR SPEEDTEST FUNCIONAL"
