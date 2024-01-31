60bdac3c09515ab599488c2d8c593e1e093908e2
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-01-30 23:45:07 -0500

    Update ooklaserver.sh

--- ooklaserver.sh
+++ ooklaserver.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 #descargamos el instalador
 wget https://install.speedtest.net/ooklaserver/ooklaserver.sh
+sleep 5
+echo "Instalador descargado"
 
 #permitimos la instalacion
-chmod a+x ooklaserver.sh
+sudo chmod a+x ooklaserver.sh
+sleep 5
+echo "Permisos concedidos"
 
 #instalamos el servidor speedtest
-./ooklaserver.sh install
+sudo ./ooklaserver.sh install
 
 #iniciamos el server
 ./ooklaserver.sh start
@@ -14,7 +18,7 @@ chmod a+x ooklaserver.sh
 #Agregamos el iniciador automatico
 cd /etc/systemd/system
 wget https://raw.githubusercontent.com/viejojavi/ooklaserver/main/OoklaServer.service
-systemctl enable OoklaServer.service
-systemctl start OoklaServer.service
+sudo systemctl enable OoklaServer.service
+sudo systemctl start OoklaServer.service
 sleep 5
 echo "SERVIDOR SPEEDTEST FUNCIONAL"
