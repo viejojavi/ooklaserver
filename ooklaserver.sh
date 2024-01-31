7df6bb247002f0428f3bd18cfade74b7eb09e75c
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-01-30 23:40:14 -0500

    Update ooklaserver.sh

--- ooklaserver.sh
+++ ooklaserver.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+#descargamos el instalador
+wget https://install.speedtest.net/ooklaserver/ooklaserver.sh
+
+#permitimos la instalacion
+chmod a+x ooklaserver.sh
+
+#instalamos el servidor speedtest
+./ooklaserver.sh install
+
+#iniciamos el server
+./ooklaserver.sh start
+
+#Agregamos el iniciador automatico
 cd /etc/systemd/system
 wget https://raw.githubusercontent.com/viejojavi/ooklaserver/main/OoklaServer.service
 systemctl enable OoklaServer.service
